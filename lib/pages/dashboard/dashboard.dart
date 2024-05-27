944fbcca13028870c3b796e60ba159ca1d0e3992
Oumaima Talbi <tb.talbioumaima@gmail.com>
2024-05-28 00:14:19 +0100

    fix dashboard design

--- lib/pages/dashboard/dashboard.dart
+++ lib/pages/dashboard/dashboard.dart
@@ -2,86 +2,71 @@ import 'package:flutter/material.dart';
 //import '/auth/firebase_auth/auth_util.dart'; // Make sure to replace this with your actual import path
 
 class Dashboard extends StatelessWidget {
- const Dashboard({super.key});
+  const Dashboard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your App Name'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              // Implement sign out functionality here
+            },
+            icon: const Icon(
+              Icons.logout,
+              size: 30,
+            ),
+            color: Colors.white, // Change the color as per your design
+          ),
+        ],
+        backgroundColor: const Color(0xFFB3CEDB), // Set the background color of the app bar
       ),
       drawer: const Drawer(
         elevation: 16,
         // Add your drawer content here
       ),
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            height: 62,
-            decoration: const BoxDecoration(
-              color: Color(0xFFB3CEDB),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                IconButton(
-                  onPressed: () async {
-                    // Implement sign out functionality here
-                  },
-                  icon: const Icon(
-                    Icons.logout,
-                    size: 30,
-                  ),
-                  color: Colors.white, // Change the color as per your design
-                ),
-              ],
-            ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(10),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        children: const [
+          DashboardGridWidget(
+            image: 'assets/images/camera1.jpg',
+            text: 'Camera',
+            route: '/camera',
           ),
-          Expanded(
-            child: GridView.count(
-              crossAxisCount: 2,
-              padding: const EdgeInsets.all(10),
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-              children: const [
-                DashboardGridWidget(
-                  image: 'assets/images/camera1.jpg',
-                  text: 'Camera',
-                  route: '/camera',
-                ),
-                DashboardGridWidget(
-                  image: 'assets/images/s0sy2_t.jpg',
-                  text: 'Temperature',
-                  route: '/temperature',
-                ),
-                DashboardGridWidget(
-                  image: 'assets/images/musique.jpg',
-                  text: 'Musique',
-                  route: '/music',
-                ),
-                DashboardGridWidget(
-                  image: 'assets/images/climatisation.jpg',
-                  text: 'Climatisation',
-                  route: '/climatisation',
-                ),
-                DashboardGridWidget(
-                  image: 'assets/images/Bulb_Logo-removebg-preview.png',
-                  text: 'Lumiére',
-                  route: '/lumiere',
-                ),
-                DashboardGridWidget(
-                  image: 'assets/images/swinng.jpg',
-                  text: 'Balancement',
-                  route: '/swing',
-                ),
-                DashboardGridWidget(
-                  image: 'assets/images/parametre.png',
-                  text: 'Paramétre',
-                  route: '/parametre',
-                ),
-              ],
-            ),
+          DashboardGridWidget(
+            image: 'assets/images/s0sy2_t.jpg',
+            text: 'Temperature',
+            route: '/temperature',
+          ),
+          DashboardGridWidget(
+            image: 'assets/images/musique.jpg',
+            text: 'Musique',
+            route: '/music',
+          ),
+          DashboardGridWidget(
+            image: 'assets/images/climatisation.jpg',
+            text: 'Climatisation',
+            route: '/climatisation',
+          ),
+          DashboardGridWidget(
+            image: 'assets/images/Bulb_Logo-removebg-preview.png',
+            text: 'Lumiére',
+            route: '/lumiere',
+          ),
+          DashboardGridWidget(
+            image: 'assets/images/swinng.jpg',
+            text: 'Balancement',
+            route: '/swing',
+          ),
+          DashboardGridWidget(
+            image: 'assets/images/parametre.png',
+            text: 'Paramétre',
+            route: '/parametre',
           ),
         ],
       ),
@@ -95,11 +80,11 @@ class DashboardGridWidget extends StatelessWidget {
   final String route;
 
   const DashboardGridWidget({
-    super.key,
+    Key? key,
     required this.image,
     required this.text,
     required this.route,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
