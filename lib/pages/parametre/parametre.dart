58fede4d6030748a9f17cbed966bea7fba22f26c
Oumaima Talbi <tb.talbioumaima@gmail.com>
2024-06-01 15:05:37 +0100

    add luncher dart to parameters

--- lib/pages/parametre/parametre.dart
+++ lib/pages/parametre/parametre.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:litbebe/services/auth_service.dart';
 
 class Parametre extends StatelessWidget {
