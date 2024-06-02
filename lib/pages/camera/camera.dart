433b3545602753fa89ad51c81a81b3c9820d3ac6
Oumaima Talbi <tb.talbioumaima@gmail.com>
2024-06-02 23:41:40 +0100

    put back camera to empty

--- lib/pages/camera/camera.dart
+++ lib/pages/camera/camera.dart
@@ -1,34 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_vlc_player/flutter_vlc_player.dart';
 
-class Camera extends StatefulWidget {
+class Camera extends StatelessWidget {
   const Camera({super.key});
   static const route = '/camera';
-
-  @override
-  _CameraState createState() => _CameraState();
-}
-
-class _CameraState extends State<Camera> {
-  late VlcPlayerController _vlcPlayerController;
-  final String streamUrl = "http://<ESP32-CAM-IP>/stream"; // Replace with your ESP32-CAM IP
-
-  @override
-  void initState() {
-    super.initState();
-    _vlcPlayerController = VlcPlayerController.network(
-      streamUrl,
-      hwAcc: HwAcc.full,
-      autoPlay: true,
-      options: VlcPlayerOptions(),
-    );
-  }
-
-  @override
-  void dispose() {
-    _vlcPlayerController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,16 +10,16 @@ class _CameraState extends State<Camera> {
       appBar: AppBar(
         title: const Text('Your App Name'),
         actions: const [
-          // IconButton(
-          //   onPressed: () async {
-          //     // Implement sign out functionality here
-          //   },
-          //   icon: const Icon(
-          //     Icons.logout,
-          //     size: 30,
-          //   ),
-          //   color: Colors.white, // Change the color as per your design
-          // ),
+          //IconButton(
+            //onPressed: () async {
+              // Implement sign out functionality here
+            //},
+            //icon: const Icon(
+            //  Icons.logout,
+             // size: 30,
+            //),
+            //color: Colors.white, // Change the color as per your design
+          //),
         ],
         backgroundColor: const Color(0xFFB3CEDB), // Set the background color of the app bar
       ),
@@ -58,13 +32,6 @@ class _CameraState extends State<Camera> {
               'assets/images/temperature-removebg-preview.png',
               height: 200,
               width: 200,
-            ),
-            Expanded(
-              child: VlcPlayer(
-                controller: _vlcPlayerController,
-                aspectRatio: 16 / 9,
-                placeholder: Center(child: CircularProgressIndicator()),
-              ),
             ),
           ],
         ),
